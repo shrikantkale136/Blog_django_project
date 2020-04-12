9c04d9da39981d2e67275aab167cc608f4f8029a
Shrikant Kale <40916158+shrikantkale136@users.noreply.github.com>
2020-04-12 19:37:17 +0530

    components created

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+#data 
+posts = [
+    {
+        'author': 'Shrikant K',
+        'title': 'Python using Django',
+        'content_type': 'pdf',
+    },
+    {
+        'author': 'Girish M',
+        'title': 'Data science',
+        'content_type': 'pdf',
+    }
+]
+
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = { 'posts' : posts, 'title': 'Home'}
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-     return render(request, 'blog/about.html')
+     return render(request, 'blog/about.html', {'title': 'About'})
 
      
