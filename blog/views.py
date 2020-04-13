8122a00e731e2769c43654d10d158aad33276900
Shrikant Kale <40916158+shrikantkale136@users.noreply.github.com>
2020-04-14 01:23:52 +0530

    done with Sqlite connection  - db model class created  - now data populates form database

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-#data 
-posts = [
-    {
-        'author': 'Shrikant K.',
-        'title': 'Django UI Framework',
-        'content': 'Learn how to make UI using Django',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Girish M.',
-        'title': 'Data Science',
-        'content': 'Learn Data Science Fundamentals.',
-        'date_posted': 'August 28, 2018'
-    }
-]
+# data
+# posts = [
+#     {
+#         'author': 'Shrikant K.',
+#         'title': 'Django UI Framework',
+#         'content': 'Learn how to make UI using Django',
+#         'date_posted': 'August 27, 2018'
+#     },
+#     {
+#         'author': 'Girish M.',
+#         'title': 'Data Science',
+#         'content': 'Learn Data Science Fundamentals.',
+#         'date_posted': 'August 28, 2018'
+#     }
+# ]
 
 # Create your views here.
+
+
 def home(request):
-    context = { 'posts' : posts, 'title': 'Home'}
+    context = {'posts': Post.objects.all(), 'title': 'Home'}
     return render(request, 'blog/home.html', context)
 
-def about(request):
-     return render(request, 'blog/about.html', {'title': 'About'})
 
-     
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
